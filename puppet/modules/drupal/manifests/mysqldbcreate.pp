62b4dc92ffdb3e95707f8212e44a09bd5ee4badb
Magnus Lindvall <magnus.lindvall@stendahls.se>
2015-09-25 15:20:57 +0200

    Remove drupal. Add GIT. Fixes #9

--- puppet/modules/drupal/manifests/mysqldbcreate.pp
+++
@@ -1,7 +0,0 @@
-define drupal::mysqldbcreate( $user, $password , $dbname ) {
-	exec { "create-drupal-db":
-		    unless => "/usr/bin/mysql -u${user} -p${password} ${dbname}",
-		    command => "/usr/bin/mysql -uroot -e \"create database ${dbname}; grant all on ${dbname}.* to ${user}@localhost identified by '${$password}';\"",
-		    require => Service["mysql"],
-	    }
-}
